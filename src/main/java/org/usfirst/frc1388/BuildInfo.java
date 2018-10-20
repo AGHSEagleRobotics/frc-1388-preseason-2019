5796ac1d32669667b1150ec6b6d8d568afa68593
Jeff Techau <jtechau@gmail.com>
2018-10-20 11:42:40 -0700

    Project created from frc-1388-2018 competition code, using WPILib: Upgrade a WPILib Eclipse project

---
+++ src/main/java/org/usfirst/frc1388/BuildInfo.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc1388;
+public class BuildInfo {
+  public static final String GIT_VERSION = "fb367f6";
+  public static final String GIT_BRANCH = "master";
+  public static final String GIT_STATUS = "";
+  public static final String BUILD_DATE = "Sat 10/20/2018 ";
+  public static final String BUILD_TIME = "11:39 AM";
+}
